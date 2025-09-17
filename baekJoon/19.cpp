d938b98d2a1c148b9486fac030fbda339ed40b1d
m2ntchoco <bwp1115000@gmail.comgit config user.email>
2025-09-17 15:03:41 +0900

    09.17 update

--- baekJoon/19.cpp
+++ baekJoon/19.cpp
@@ -15,9 +15,17 @@
 //   return 0;
 // }
 
-#include <iostream>
+#include <iostream> 
 
+using namespace std;
 int main(){
-  int a;
-  cin.tie(Null) >> a;
+  ios_base::sync_with_stdio(false); //C와 C++의 입출력 동기화를 해제하여 속도 향상
+    cin.tie(NULL);
+  int a =0, b = 0, c =0;
+  cin >> a;
+for (int i = 0; i < a; i++){
+    cin >> b >> c;
+    cout << b + c << "\n";
+}
+  return 0;
 }
