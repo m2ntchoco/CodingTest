14a819d019429aa703058c486e86f341c5b898e2
m2ntchoco <bwp1115000@gmail.comgit config user.email>
2025-10-02 11:27:06 +0900

    10.02

--- baekJoon/28.dSYM/Contents/40.cpp
+++ baekJoon/28.dSYM/Contents/40.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 int main(){
 
-  vector<char> S;
+  string S;
+  vector<int> str(26, -1);
+  cin >> S;
 
+  for (int i = 0; i < S.length(); i++) {
+    int idx = S[i] - 'a';  
+    if (str[idx] == -1) 
+    { 
+      str[idx] = i;
+    }
+}
   
+  for(int i = 0; i < 26; i++){
+    cout << str[i] << " ";
+  }
 
+  return 0;
 }
